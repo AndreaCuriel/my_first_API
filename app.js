3c1ffc8edf90eac1295e9d7e7486a6f71f0e9dd5
AndreaCuriel <acurielipn@hotmail.com>
2022-04-24 15:20:28 -0500

    7) HTTP methods (GET) endpoint que regrese un explorer mediando un ID

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ app.get("/v1/explorers", (req, res) => {
   res.status(200).json(explorers);
 });
 
+//Get, crear un endpoint que te regrese un explorer mediando un ID
+app.get("/v1/explorers/:id", (req, res) => {
+  console.log(`Api Explorers GET request ${new Date()}`);
+  console.log(`Getting explorer with id ${req.params.id}`);
+  const explorer = { id: 1, name: "Andrea" };
+  res.status(200).json(explorer);
+});
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
